36465896fc4c65aebcc7e10f417fb8db18dae2ad
Владимир Сулым <v.sulym@gmail.com>
2024-11-09 11:32:35 +0300

    there was a consultation on the course

--- main.py
+++ main.py
@@ -1,62 +1,230 @@
+import requests
+
 from src.api_hh import ApiHH
 from src.db_manager import DBManager
 from src.employer import Employer
 from src.utils import search_employers_on_hh
 from src.vacancy import Vacancy
 
+
 def main():
-    print ("Добро пожаловать в Проект 3. Поиск вакансий с подключением БД.")
+    print("Добро пожаловать в Проект 3. Поиск вакансий с подключением БД.")
     value_menu = None
     show_values = 10
+    db_manager = DBManager()
+    skipping_menu_item = False
+    full_show = False
     while value_menu != "0":
-        print(f'\nГЛАВНОЕ МЕНЮ:\n'
-              f'1. Найти работодателя на НН.ru\n'
-              f'2. Найти вакансию на НН.ru\n'
-              f'3. Работа с локальной БД которая содержит:\n'
-              f'   Вакансий {10}\n'
-              f'   Компаний {10}\n'
-              f'9. Изменить кол-во значений для показа. Установлено {show_values}\n'
-              f'0. Выход')
-        value_menu = input('введите пункт меню => ')
+        if skipping_menu_item is False:
+            print(f'\nГЛАВНОЕ МЕНЮ:\n'
+                  f'1. Найти работодателя на НН.ru\n'
+                  f'2. Найти вакансию на НН.ru\n'
+                  f'3. Работа с локальной БД которая содержит:\n'
+                  f'   Вакансий {db_manager.count_vacancies}\n'
+                  f'   Компаний {db_manager.count_employers}\n'
+                  f'9. Изменить кол-во значений для показа. (Установлено {show_values})\n'
+                  f'0. Выход')
+            value_menu = input('введите пункт меню => ')
+        skipping_menu_item = False
 
         match value_menu:
             case "1":
                 value_menu_1 = None
-                overflow_show = False
                 user_input = input('Введите название компании для поиска => ')
-                print('')
                 employers = search_employers_on_hh(user_input)
                 if employers:
-                    print(f'Найдено компаний: {len(employers)}')
+                    print(f'\nНайдено компаний: {len(employers)}\n')
                     for i in range(show_values if len(employers) > show_values else len(employers)):
                         print(employers[i])
+                    while value_menu_1 != "0":
+                        print(f'\nМЕНЮ РАБОТЫ С НАЙДЕННЫМИ КОМПАНИЯМИ:')
+                        print('1. Сохранить найденных работодателей в локальной БД')
+                        print('2. Показать вакансии выбранного работодателя')
+                        print('3. Повторить поиск работодателя на сайте HH.ru')
+                        if len(employers) > show_values and full_show is False:
+                            print(f'4. Показать ВСЕ компании (найдено {len(employers)} компаний, показано {show_values})')
+                        full_show = False
+                        print('0. Возврат в Главное Меню')
+                        value_menu_1 = input('введите пункт меню => ')
+                        match value_menu_1:
+                            case "1":
+                                db_manager.employers = employers
+                                DBManager()
+                            case "2":
+                                input_id_user = input("Введите ID работодателя => ")
+                                print('')
+                                for employer in employers:
+                                    if input_id_user == employer.employer['id']:
+                                        response = requests.get(employer.employer['vacancies_url'])
+                                        vacancies = response.json()["items"]
+                                        for vacancy in vacancies:
+                                            print(Vacancy(vacancy))
+
+                            case "3":
+                                value_menu_1 = "0"
+                                value_menu = "1"
+                                skipping_menu_item = True
+                            case "4":
+                                for employer in employers:
+                                    print(employer)
+                                full_show = True
+                            case "0":
+                                pass
+                            case _:
+                                print("Нет такого пункта, повторите ввод")
                 else:
                     print(f'Компания {user_input} не найдена на HH.ru')
-                while value_menu_1 != "0":
-                    print(f'\nМЕНЮ РАБОТЫ С НАЙДЕННЫМИ КОМПАНИЯМИ:')
-                    print('1. Сохранить найденных работодателей в локальной БД')
-                    print('2. Показать вакансии выбранного работодателя')
-                    print('3. Повторить поиск работодателя на сайте HH.ru')
-                    if len(employers) > show_values:
-                        print(f'4. Показать ВСЕ компании (найдено {len(employers)}, показано {show_values})')
-                    print('0. Возврат в Главное Меню')
-                    value_menu_1 = input('введите пункт меню => ')
-                    match value_menu_1:
-                        case "1":
-                            pass
-                        case "2":
-                            pass
-                        case "3":
-                            pass
-                        case "0":
-                            pass
-                        case _:
-                            print("Нет такого пункта, повторите ввод")
 
             case "2":
                 pass
             case "3":
-                pass
+                value_menu_3 = None
+                while value_menu_3 != "0":
+                    print("\nМЕНЮ РАБОТЫ С ЛОКАЛЬНОЙ БД:")
+                    print(f'1. Показать список компаний и количество вакансий.\n'
+                          f'2. Показать список всех вакансий с указанием названия компании, '
+                          f'названия вакансии и зарплаты и ссылки на вакансию.\n'
+                          f'3. Показать среднюю зарплату по всем вакансиям.\n'
+                          f'4. Показать вакансий, у которых зарплата выше средней по всем вакансиям.\n'
+                          f'5. Найти вакансии по ключевому слову в названии')
+                    print(f'9. Изменить кол-во значений для показа. (Установлено {show_values})\n'
+                          f'0. Вернуться в Главное Меню')
+                    value_menu_3 = input('введите пункт меню => ')
+                    match value_menu_3:
+                        case "1":
+                            companies_and_vacancies_count = db_manager.get_companies_and_vacancies_count()
+                            print('')
+                            if companies_and_vacancies_count:
+                                for i in range(show_values if len(companies_and_vacancies_count) > show_values else len(
+                                        companies_and_vacancies_count)):
+                                    print(
+                                        f'Компания: {companies_and_vacancies_count[i][0]}, содержит вакансий: '
+                                        f'{companies_and_vacancies_count[i][1]}')
+                            if len(companies_and_vacancies_count) > show_values:
+                                print(f'\n1. Показать ВСЕ компании (найдено {len(companies_and_vacancies_count)} '
+                                      f'уникальных названий компаний, показано {show_values})\n'
+                                      f'9. Изменить кол-во значений для показа. (Установлено {show_values})\n'
+                                      f'0. Вернуться в Меню Работы с Локальной БД')
+                                value_menu_3_1 = input('введите пункт меню => ')
+                                match value_menu_3_1:
+                                    case "1":
+                                        for value in companies_and_vacancies_count:
+                                            print(f'Компания: {value[0]}, содержит вакансий: {value[1]}')
+                                    case "9":
+                                        show_values = int(input('Введите количество значений для показа => '))
+                                    case "0":
+                                        pass
+                                    case _:
+                                        pass
+
+                        case "2":
+                            list_vacancies_found = db_manager.get_all_vacancies()
+                            print('')
+                            if list_vacancies_found:
+                                for i in range(show_values if len(list_vacancies_found) > show_values else len(
+                                        list_vacancies_found)):
+                                    print(
+                                        f'\nКомпания: {list_vacancies_found[i][0]},\n'
+                                        f'Вакансий: {list_vacancies_found[i][1]}\n'
+                                        f'Зарплата от {list_vacancies_found[i][2]} до {list_vacancies_found[i][3]}\n'
+                                        f'URL: {list_vacancies_found[i][4]}')
+                                if len(list_vacancies_found) > show_values:
+                                    print(f'\n1. Показать ВСЕ вакансии (найдено {len(list_vacancies_found)} '
+                                          f'вакансий, показано {show_values})\n'
+                                          f'9. Изменить кол-во значений для показа. (Установлено {show_values})\n'
+                                          f'0. Вернуться в Меню Работы с Локальной БД')
+                                    value_menu_3_2 = input('введите пункт меню => ')
+                                    match value_menu_3_2:
+                                        case "1":
+                                            for value in list_vacancies_found:
+                                                print(
+                                                    f'Компания: {value[0]},\n'
+                                                    f'Вакансий: {value[1]}\n'
+                                                    f'Зарплата от {value[2]} до {value[3]}\n'
+                                                    f'URL: {value[4]}')
+                                        case "9":
+                                            show_values = int(input('Введите количество значений для показа => '))
+                                        case "0":
+                                            pass
+                                        case _:
+                                            pass
+                        case "3":
+                            avg_salary = db_manager.get_avg_salary()
+                            print('')
+                            if avg_salary:
+                                print(f'Средняя ЗП по всем вакансиям в локальной БД = {avg_salary}')
+                        case "4":
+                            vacancies_above_avg = db_manager.get_vacancies_with_higher_salary()
+                            print('')
+                            if vacancies_above_avg:
+                                print()
+                                for i in range(show_values if len(vacancies_above_avg) > show_values else len(
+                                        vacancies_above_avg)):
+                                    print(
+                                        f'\nID: {vacancies_above_avg[i][0]},\n'
+                                        f'Вакансий: {vacancies_above_avg[i][1]}\n'
+                                        f'Зарплата от {vacancies_above_avg[i][2]} до {vacancies_above_avg[i][3]}\n'
+                                        f'URL: {vacancies_above_avg[i][4]}')
+                                if len(vacancies_above_avg) > show_values:
+                                    print(f'\n1. Показать ВСЕ вакансии (найдено {len(vacancies_above_avg)} '
+                                          f'вакансий, показано {show_values})\n'
+                                          f'9. Изменить кол-во значений для показа. (Установлено {show_values})\n'
+                                          f'0. Вернуться в Меню Работы с Локальной БД')
+                                    value_menu_3_4 = input('введите пункт меню => ')
+                                    match value_menu_3_4:
+                                        case "1":
+                                            for value in vacancies_above_avg:
+                                                print(
+                                                    f'ID: {value[0]},\n'
+                                                    f'Вакансий: {value[1]}\n'
+                                                    f'Зарплата от {value[2]} до {value[3]}\n'
+                                                    f'URL: {value[4]}')
+                                        case "9":
+                                            show_values = int(input('Введите количество значений для показа => '))
+                                        case "0":
+                                            pass
+                                        case _:
+                                            pass
+                        case "5":
+                            keyword = input('Введиет ключевое слово для поиска => ')
+                            vacancies_by_keyword = db_manager.get_vacancies_with_keyword(keyword)
+                            print('')
+                            if vacancies_by_keyword:
+                                for i in range(show_values if len(vacancies_by_keyword) > show_values else len(
+                                        vacancies_by_keyword)):
+                                    print(
+                                        f'\nID: {vacancies_by_keyword[i][0]},\n'
+                                        f'Вакансий: {vacancies_by_keyword[i][1]}\n'
+                                        f'Зарплата от {vacancies_by_keyword[i][2]} до {vacancies_by_keyword[i][3]}\n'
+                                        f'URL: {vacancies_by_keyword[i][4]}')
+                                if len(vacancies_by_keyword) > show_values:
+                                    print(f'\n1. Показать ВСЕ вакансии (найдено {len(vacancies_by_keyword)} '
+                                          f'вакансий, показано {show_values})\n'
+                                          f'9. Изменить кол-во значений для показа. (Установлено {show_values})\n'
+                                          f'0. Вернуться в Меню Работы с Локальной БД')
+                                    value_menu_3_5 = input('введите пункт меню => ')
+                                    match value_menu_3_5:
+                                        case "1":
+                                            for value in vacancies_by_keyword:
+                                                print(
+                                                    f'ID: {value[0]},\n'
+                                                    f'Вакансий: {value[1]}\n'
+                                                    f'Зарплата от {value[2]} до {value[3]}\n'
+                                                    f'URL: {value[4]}')
+                                        case "9":
+                                            show_values = int(input('Введите количество значений для показа => '))
+                                        case "0":
+                                            pass
+                                        case _:
+                                            pass
+                            else:
+                                print(f'Вакансий по запросу - {keyword} - не найдено.')
+                        case "9":
+                            show_values = int(input('Введите количество значений для показа => '))
+                        case "0":
+                            pass
+                        case _:
+                            print("Нет такого пункта, повторите ввод")
             case "9":
                 show_values = int(input('Введите количество значений для показа => '))
             case "0":
--- src/db_manager.py
+++ src/db_manager.py
@@ -1,47 +1,60 @@
+import logging
+
 import psycopg2
 
+from config import LOG_FORMAT, LOG_LEVEL
+
+logger = logging.getLogger(__name__)
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(LOG_FORMAT)
+logger.addHandler(console_handler)
+logger.setLevel(LOG_LEVEL)
 
 class DBManager:
     """
     Класс для подключения и работы с базой данных SQl вакансий HH
     """
 
-    def __init__(self, name_db='hh_db'):
-        self.__conn_params = {
-            'host': "localhost",
-            'port': 5432,
-            'user': "postgres",
-            'password': "qwerty123",
-        }
-        self.name_db = name_db
-        self.__creating_database()
-        self.__init_table()
-        self.__count_vacancies = 0
-        self.__count_employers = 0
+    conn_params = {
+        'host': "localhost",
+        'port': 5432,
+        'user': "postgres",
+        'password': "qwerty123",
+    }
+    name_db = ''
+    count_vacancies = 0
+    count_employers = 0
 
-    def __creating_database(self):
+    def __init__(self, name_db='hh_db'):
+        self.creating_database()
+        self.init_table()
+
+    @classmethod
+    def creating_database(cls, name_db='hh_db'):
         """
         Функция создает базу данных на сервере SQL
         """
         try:
-            conn = psycopg2.connect(**self.__conn_params)
+            cls.name_db = name_db
+            conn = psycopg2.connect(**cls.conn_params)
             cursor = conn.cursor()
             conn.autocommit = True  # Благодаря этому команда SQL, Во-первых, выполняется немедленно.
             # А во-вторых, выполняется вне транзакции
             # (выражение "CREATE DATABASE" должно выполняться именно вне транзакции)
-            cursor.execute(f"CREATE DATABASE {self.name_db}")
-            print(f"База данных {self.name_db} успешно создана")
+            cursor.execute(f"CREATE DATABASE {name_db}")
+            print(f"База данных {name_db} успешно создана")
             cursor.close()
             conn.close()
         except Exception as e:
-            print(e)
+            logger.info(e)
 
-    def __init_table(self):
+    @classmethod
+    def init_table(cls):
         """
         Функция создает основные таблицы в базе данных которую создал данный класс
         """
-        self.__conn_params['database'] = self.name_db
-        with psycopg2.connect(**self.__conn_params) as conn:
+        cls.conn_params['database'] = cls.name_db
+        with psycopg2.connect(**cls.conn_params) as conn:
             # conn.autocommit = True
             with conn.cursor() as cursor:
                 # проверяем, существует ли такая таблица
@@ -59,9 +72,8 @@ class DBManager:
                                    "")
                 else:
                     cursor.execute("SELECT COUNT(*) FROM employers")
-                    self.__count_employers = cursor.fetchone()[0]
-                    print(self.__count_employers)
-                    print("Таблица employers существует")
+                    cls.count_employers = cursor.fetchone()[0]
+                    logger.info(f"Таблица employers существует и содержит {cls.count_employers} записей")
 
 
                 cursor.execute("select * from information_schema.tables where table_name=%s", ('vacancies',))
@@ -85,20 +97,24 @@ class DBManager:
                     conn.commit()
                 else:
                     cursor.execute("SELECT COUNT(*) FROM vacancies")
-                    self.__count_vacancies = cursor.fetchone()[0]
-                    print(self.__count_vacancies)
-                    print("Таблица vacancies существует")
+                    cls.count_vacancies = cursor.fetchone()[0]
+                    logger.info(f"Таблица vacancies существует и содержит {cls.count_vacancies} записей")
+
         conn.close()
 
     @property
     def vacancies(self):
         return None
 
+    @property
+    def employers(self):
+        return None
+
     @vacancies.setter
     def vacancies(self, vacancies: list):
-        print('запущен сетер')
-        self.__conn_params['database'] = self.name_db
-        with psycopg2.connect(**self.__conn_params) as conn:
+        logger.debug(f'start => vacancies.setter')
+        # DBManager.conn_params['database'] = DBManager.name_db
+        with psycopg2.connect(**DBManager.conn_params) as conn:
             with conn.cursor() as cursor:
                 for vacancy in vacancies:
                     try:
@@ -115,15 +131,118 @@ class DBManager:
                     conn.commit()
         conn.close()
 
+    @employers.setter
+    def employers(self, employers: list):
+        logger.debug(f'start => employers.setter')
+        with psycopg2.connect(**DBManager.conn_params) as conn:
+            with conn.cursor() as cursor:
+                for employer in employers:
+                    try:
+                        cursor.execute("INSERT INTO employers VALUES (%s, %s, %s, %s)",
+                                       (list(employer.employer.values())))
+                        print('Данные сохранены в БД')
+                    except Exception as e:
+                        print (e.pgerror)
+                        print (e.diag.message_detail)
+                    conn.commit()
+        conn.close()
 
-# get_companies_and_vacancies_count() — получает список всех компаний и количество вакансий у каждой компании.
-# get_all_vacancies() — получает список всех вакансий с указанием названия компании, названия вакансии и зарплаты и ссылки на вакансию.
-# get_avg_salary() — получает среднюю зарплату по вакансиям.
-# get_vacancies_with_higher_salary() — получает список всех вакансий, у которых зарплата выше средней по всем вакансиям.
-# get_vacancies_with_keyword() — получает список всех вакансий, в названии которых содержатся переданные в метод слова, например python.
+    @staticmethod
+    def get_companies_and_vacancies_count():
+        """Функция получает из локальной БД список всех компаний и количество вакансий у каждой компании"""
+        with psycopg2.connect(**DBManager.conn_params) as conn:
+            with conn.cursor() as cursor:
+                try:
+                    cursor.execute("SELECT "
+                                   "employers.name, "
+                                   "COUNT(vacancies) as count FROM employers "
+                                   "JOIN vacancies ON employers.id = vacancies.employer_id "
+                                   "GROUP BY employers.name")
+                    return cursor.fetchall()
+                except Exception as e:
+                    print(e)
+                conn.commit()
+        conn.close()
+
+    @staticmethod
+    def get_all_vacancies():
+        """Функция получает из локальной БД список всех вакансий с указанием названия компании,
+        названия вакансии и зарплаты и ссылки на вакансию."""
+        with psycopg2.connect(**DBManager.conn_params) as conn:
+            with conn.cursor() as cursor:
+                try:
+                    cursor.execute("SELECT "
+                                   "employers.name as name_company,"
+                                   "vacancies.name as vacancy,"
+                                   "salary_from,"
+                                   "salary_to,"
+                                   "vacancies.url "
+                                   "FROM vacancies "
+                                   "JOIN employers ON employers.id = vacancies.employer_id")
+                    return cursor.fetchall()
+                except Exception as e:
+                    print(e)
+                conn.commit()
+        conn.close()
+
+    @staticmethod
+    def get_avg_salary():
+        """Функция получает из локальной БД среднюю зарплату по вакансиям."""
+        with psycopg2.connect(**DBManager.conn_params) as conn:
+            with conn.cursor() as cursor:
+                try:
+                    cursor.execute("SELECT AVG(salary_from) FROM vacancies")
+                    return round(cursor.fetchall()[0][0], 2)
+                except Exception as e:
+                    print(e)
+                conn.commit()
+        conn.close()
+
+
+    @staticmethod
+    def get_vacancies_with_higher_salary():
+        """Функция получает список всех вакансий, у которых зарплата выше средней по всем вакансиям."""
+        with psycopg2.connect(**DBManager.conn_params) as conn:
+            with conn.cursor() as cursor:
+                try:
+                    cursor.execute("SELECT "
+                                   "id, "
+                                   "name, "
+                                   "salary_from, "
+                                   "salary_to, "
+                                   "url "
+                                   "FROM vacancies "
+                                   "WHERE salary_from > (SELECT AVG(salary_from) from vacancies)")
+                    return cursor.fetchall()
+                except Exception as e:
+                    print(e)
+                conn.commit()
+        conn.close()
+
+
+    @staticmethod
+    def get_vacancies_with_keyword(search_str):
+        """Функция получает список всех вакансий из локальной БД, в названии которых содержатся
+        переданные в метод слова, например python."""
+        with psycopg2.connect(**DBManager.conn_params) as conn:
+            with conn.cursor() as cursor:
+                try:
+                    cursor.execute(f"SELECT "
+                                   f"id, "
+                                   f"name, "
+                                   f"salary_from, "
+                                   f"salary_to, "
+                                   f"url FROM vacancies WHERE name LIKE '%{search_str}%'")
+                    return cursor.fetchall()
+                except Exception as e:
+                    print(e)
+                conn.commit()
+        conn.close()
+
 
 if __name__ == '__main__':
-    DBManager()
+    db_manager = DBManager()
+    print(db_manager.get_vacancies_with_keyword('python'))
 
     # with connect_db() as conn:
     #     with conn.cursor() as cur:
